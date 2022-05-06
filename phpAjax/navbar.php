a1688ab7eabfe2f98a5ab7207f0fd5152ee7e53a
panditprogrammer <panditprogrammer@gmail.com>
2022-05-06 12:12:19 +0530

    pagination with php ajax

--- phpAjax/navbar.php
+++ phpAjax/navbar.php
@@ -3,6 +3,7 @@
     <div class="row">
         <div class="col">
             <a class="btn-sm btn bg-white" href="phpAjax-fetch_00.php">fetch Data</a>
+            <a class="btn-sm btn bg-white" href="phpAjax-pagination_05.php">pagination</a>
             <a class="btn-sm btn bg-white" href="phpAjax-insert_01.php">Insert Data</a>
         </div>
     </div>
